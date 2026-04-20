5c993b659e02560711995201e2673b851e9a1c6f
Alexander Wittmann (Nokia) <alexander.wittmann@nokia.com>
2026-04-20 11:35:20 +0200

    v0.3 MVP - nutrition coach planner foundation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,19 +1,36 @@
 //Import React state hook
 //useState lets us store and update values inside the component 
-import { useState } from "react";
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useEffect, useState } from "react";
+
 
 // Import React Native UI components and helpers
 import {
   Button, // Container (like a div)
   KeyboardAvoidingView, // Display text
   Platform,
-  Pressable, // Input field
-  StyleSheet, // Button
+  Pressable, // Button
+  ScrollView, // Input field
+  StyleSheet,
   Text, // Styling system
   TextInput, // Prevent keyboard from converting inputs
   View,
 } from 'react-native';
 
+type Entry = {
+    id: string;
+    date: string;
+    garminBurn: number;
+    appleBurn: number;
+    intake: number;
+    trainingType: 'strength' | 'running' | 'hiit';
+    realBurn: number;
+    deficit: number;
+    notes: string;
+};
+
+const STORAGE_KEY = 'entries';
 
 // Main screen component
 export default function HomeScreen() {
@@ -35,25 +52,33 @@ export default function HomeScreen() {
   // null = no result yet 
   const [realBurn, setRealBurn] = useState<number | null>(null);
   const [deficit, setDeficit] = useState<number | null>(null);
+  
+  const [entries, setEntries] = useState<Entry[]>([]);
+
+
+
+  useEffect(() => {
+    const loadEntries = async () => {
+      try{
+        const stored = await AsyncStorage.getItem(STORAGE_KEY);
+        if (stored) {
+          setEntries(JSON.parse(stored));
+        }
+      } catch (error) {
+        console.log('Error loading entries', error);
+      }
+    };
+
+    loadEntries();
+  }, []);
+
 
   //--------------------------------------------
   // SAVED ENTRIES STATE
   //--------------------------------------------
   // Stores all saved daily logs (history of entries)
 
-  type Entry = {
-    date: string;
-    garminBurn: number;
-    appleBurn: number;
-    intake: number;
-    trainingType: 'strength' | 'running' | 'hiit';
-    realBurn: number;
-    deficit: number;
-    notes: string;
-  };
-
-  const [entries, setEntries] = useState<Entry[]>([]);
-
+  
   // ------------------------------------------
   // RESULT STATE
   // ------------------------------------------
@@ -91,11 +116,12 @@ export default function HomeScreen() {
     setDeficit(def);
   };
 
-  const handleSaveEntry = () => {
+  const handleSaveEntry = async () => {
     if (realBurn === null || deficit === null) return;
 
     const entry: Entry = {
-      date: '', // later should be more dynamicly 
+      id: Date.now().toString(),
+      date: new Date().toISOString() , // later should be more dynamicly ---> done
       garminBurn: Number(garmin),
       appleBurn: Number(apple),
       intake: Number(intake),
@@ -105,8 +131,28 @@ export default function HomeScreen() {
       notes: '',
     };
 
-    setEntries((prev) => [entry, ...prev]);
+    const updatedEntries = [entry, ...entries];
+
+    setEntries (updatedEntries);
+
+    try {
+      await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(updatedEntries));
+    } catch (error) {
+      console.log('Error saving entries:', error);
+    }
   };
+
+  const handleDeleteEntry = async (id: string) => {
+    const updatedEntries = entries.filter((entry) => entry.id !== id);
+    setEntries(updatedEntries);
+
+    try{
+      await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(updatedEntries));
+    } catch (error) {
+      console.log('Error deleting entry:', error);
+    }
+  };  
+  
   // ------------------------------------------
   // UI RENDER
   // ------------------------------------------
@@ -115,102 +161,117 @@ export default function HomeScreen() {
     style={styles.container}
     behavior={Platform.OS === 'ios' ? 'padding' : undefined}
     >
-      <Text style={styles.title}>Kcal Rechner</Text>
-      <TextInput 
+      <ScrollView
+        contentContainerStyle = {styles.scrollContent}
+        keyboardShouldPersistTaps = "handled"
+      >
+
+    
+        <Text style={styles.title}>Kcal Rechner</Text>
+        <TextInput 
+          style = {styles.input}
+          placeholder="Garmin Burn"
+          placeholderTextColor="#8e8e93"
+          keyboardType="numeric"
+          value={garmin}
+          onChangeText={setGarmin}
+        />
+
+        <TextInput 
         style = {styles.input}
-        placeholder="Garmin Burn"
+        placeholder="Apple Burn"
         placeholderTextColor="#8e8e93"
         keyboardType="numeric"
-        value={garmin}
-        onChangeText={setGarmin}
-    />
+        value={apple}
+        onChangeText={setApple}
+        />
 
-    <TextInput 
-      style = {styles.input}
-      placeholder="Apple Burn"
-      placeholderTextColor="#8e8e93"
-      keyboardType="numeric"
-      value={apple}
-      onChangeText={setApple}
-    />
-
-    <TextInput
-    style={styles.input}
-    placeholder="Intake"
-    placeholderTextColor="#8e8e93"
-    keyboardType="numeric"
-    value={intake}
-    onChangeText={setIntake}
-    />
-    <Text style={styles.sectionTitle}>Training Type</Text>
-      <View style={styles.buttonRow}>
-        {(['strength', 'running', 'hiit']as const).map((type) => (
-        <Pressable 
-          key={type}
-          onPress={() => setTrainingType(type)}
-          style={({ pressed }) => [
-            styles.typeButton,
-            trainingType === type && styles.typeButtonActive,
-            pressed && styles.typeButtonPressed, 
-          ]}
-        >
-          <Text
-            style={[
-              styles.typeButtonText,
-              trainingType === type && styles.typeButtonTextActive,
+        <TextInput
+        style={styles.input}
+        placeholder="Intake"
+        placeholderTextColor="#8e8e93"
+        keyboardType="numeric"
+        value={intake}
+        onChangeText={setIntake}
+        />
+        <Text style={styles.sectionTitle}>Training Type</Text>
+          <View style={styles.buttonRow}>
+            {(['strength', 'running', 'hiit']as const).map((type) => (
+            <Pressable 
+             key={type}
+              onPress={() => setTrainingType(type)}
+              style={({ pressed }) => [
+              styles.typeButton,
+              trainingType === type && styles.typeButtonActive,
+              pressed && styles.typeButtonPressed, 
             ]}
-        >
-            {type.toUpperCase()}
-          </Text>
-        </Pressable>
-        ))}
-      </View>
-
-
-    <Button title="Calculate" onPress={handleCalculate} />
-
-    {realBurn !== null && (
-      <View style={styles.result}>
-        <Text style={styles.resultText}>
-          Real Burn: {realBurn.toFixed(0)} kcal 
-        </Text>
-        <Text style={styles.resultText}>
-          Deficit: {deficit?.toFixed(0)} kcal
-        </Text>
-
-       
-      <Button 
-      title="Save Entry" 
-      onPress={handleSaveEntry} 
-      disabled={realBurn === null}
-      />
-      </View>
-     )}
-
-    {entries.length > 0 && (
-      <View style = {{ marginTop: 20 }}>
-        {entries.map ((entry, index) => (
-          <View 
-            key={index}
-            style={{
-              padding: 12,
-              borderWidth: 1,
-              borderColor: '#ddd',
-              borderRadius: 10,
-              marginBottom: 10,
-              backgroundColor: 'ffffff',
-            }}
-          > 
-            <Text style = {{ fontWeight: '600', color: '#111'}}>
-              {entry.trainingType.toUpperCase()}
+          >
+            <Text
+              style={[
+                styles.typeButtonText,
+                trainingType === type && styles.typeButtonTextActive,
+              ]}
+          >
+              {type.toUpperCase()}
             </Text>
+          </Pressable>
+          ))}
+        </View>
+
+
+        <Button title="Calculate" onPress={handleCalculate} />
+
+        {realBurn !== null && (
+          <View style={styles.result}>
+            <Text style={styles.resultText}>
+              Real Burn: {realBurn.toFixed(0)} kcal 
+            </Text>
+            <Text style={styles.resultText}>
+             Deficit: {deficit?.toFixed(0)} kcal
+            </Text>
+
         
-            <Text>Real Burn: {entry.realBurn.toFixed(0)} kcal </Text>
-            <Text> Deficit: {entry.deficit.toFixed(0)} kcal</Text>
+        <Button 
+        title="Save Entry" 
+        onPress={handleSaveEntry} 
+        disabled={realBurn === null}
+        />
+        </View>
+        )}
+
+
+
+        {entries.length > 0 && (
+         <View style = {{ marginTop: 20 }}>
+            {entries.map ((entry, index) => (
+              <View 
+               key={entry.id}
+                style={{
+                  padding: 12,
+                  borderWidth: 1,
+                  borderColor: '#ddd',
+                  borderRadius: 10,
+                  marginBottom: 10,
+                  backgroundColor: 'ffffff',
+                }}
+              > 
+                <Text style = {{ color: '#666', marginBottom: 6}}>
+                  {new Date(entry.date).toLocaleString('de-DE')}
+                </Text>
+
+                <Text style = {{ fontWeight: '600', color: '#111'}}>
+                  {entry.trainingType.toUpperCase()}
+                </Text>
+          
+                <Text>Real Burn: {entry.realBurn.toFixed(0)} kcal </Text>
+                <Text> Deficit: {entry.deficit.toFixed(0)} kcal</Text>
+              
+                <Button title="Delete" onPress={() => handleDeleteEntry(entry.id)}/>
+        
+              </View>
+            ))}
           </View>
-        ))}
-      </View>
-    )}
+    )}</ScrollView>
     </KeyboardAvoidingView>
   );
 }
@@ -306,6 +367,10 @@ const styles = StyleSheet.create({
     color: '#000000',
     fontWeight: '700',
     fontSize: 14,
+  },
+
+  scrollContent: {
+    paddingBottom: 40,
   },
 
 });
